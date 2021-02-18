ebcccc6515e4f1c2e33c51c64bab3171fca4cbd5
morayodev <70142247+morayodev@users.noreply.github.com>
2021-02-18 14:06:53 +0100

    Add files via upload
    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import Builder from "./components/Builder"
-import LogIn from "./components/LogIn"
-import  SignIn from "./components/SignIn"
+import Builder from "./components/LandingPage/Builder"
+// import Login1 from "./components/Login/Login1"
+import SignIn from "./components/Login/SignIn"
+import LogIn from "./components/Login/LogIn";
+import SignIn1 from "./components/Login/SignIn1"
+import { AuthProvider } from "./Context/AuthContex";
+
 
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Switch>
         <Route exact path="/" component={Builder} />
+        {/* <Route exact path="/Login1" component={Login1} /> */}
         <Route exact path="/LogIn" component={LogIn} />
-        <Route exact path="/SignIn" component={SignIn} />
+
+        {/* <Route exact path="/SignIn" component={SignIn} /> */}
+        <Route exact path="/SignIn1" component={SignIn1} />
       </Switch>
-   
-    </>
+      {/* <Login1/> */}
+    </AuthProvider>
   );
-}
+}  
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ import './assets/css/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore,applyMiddleware,compose,combineReducers}  from "redux"
+// import auth from "./store/auth"
+import thunk from "redux-thunk";
 
 
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({
+  // burgerBuilder: reducer,
+  // actions: orderReducer,
+  // authReducer: auth,
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
