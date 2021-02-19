25105fc3e93df2120d5fa04307eed9b928df764f
morayodev <70142247+morayodev@users.noreply.github.com>
2021-02-19 14:11:11 +0100

    Add files via upload

---
+++ src/components/LandingPage/Builder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from "./LandingPage"
+
+const Builder = () => {
+    return (
+        <div>
+            <LandingPage/> 
+        </div>
+    )
+}
+
+export default Builder;
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import logo from "../images/logo 2.png"
+import "../Login/LandingPage.css"
+import Man from "../images/Saly-14.png"
+import Vector from "../images/Vector.png"
+import { Link } from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi"
+// import PrivateRoute from "./components/PrivateRoute/PrivateRoute"
+
+
+function LandingPage() {
+
+    return (
+      <div className="mx-0 md:mx-20">
+        <div className="flex justify-between">
+          <img src={logo} alt="logo" />
+          <div
+            className="block md:hidden m-5 text-2xl"
+            style={{ color: "rgba(169, 82, 242, 1)" }}
+          >
+            <GiHamburgerMenu />
+          </div>
+          <div className="hidden md:flex md:items-center ">
+            <Link to="/LogIn">
+              <button className="mx-5 border rounded-lg LogIn p-5 md:px-10 md:py-5 text-white">
+                Login
+              </button>
+            </Link>
+            <Link to="/SignIn1">
+              <button className="mx-5 border rounded-lg SignIn  p-5 md:px-10 md:py-5">
+                Sign up
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex justify-between text-center md:text-left">
+          <div className="w-full mt-32 md:w-4/12">
+            <h1 className="font-semibold text-2xl md:text-3xl ml-5">
+              Send <span className="text-orange-600">Airtime</span> and
+              <span className="text-orange-600"> Data</span> to your team.
+            </h1>
+            <p className="font-semibold text-2xl md:text-3xl ml-5 mb-10">
+              Anytime.Anyday
+            </p>
+            <Link to="/Dashbord">
+              <button className="mx-5 border rounded-lg LogIn px-10 py-5 text-white">
+                Get Started
+              </button>
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <img className="Vector" src={Vector} alt="Vector" />
+            <img className="Man" src={Man} alt="Man" />
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default LandingPage
