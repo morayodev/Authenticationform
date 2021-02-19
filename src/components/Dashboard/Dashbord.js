0ddd24a08ab564a3ca3ed16a72ebc76cd56218b9
morayodev <70142247+morayodev@users.noreply.github.com>
2021-02-19 14:14:13 +0100

    Add files via upload

---
+++ src/components/Dashboard/Dashbord.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import Sidebar from "../Sidebar/Sidebar"
+
+function Dashbord() {
+  return (
+    <>
+      <div className="flex">
+        <div>
+          <Sidebar />
+        </div>
+
+        <div>
+          <div
+            className=""
+            // style={{ width: "80%" }}
+          >
+            <p>Over view</p>
+            <button>Move Commission</button>
+          </div>
+          <p>hjgjhuj</p>
+        </div>
+
+        {/* <div class="main-container"> */}
+        {/* <div class="sidesec">
+        <div class="slide">
+          <p class="main-header">Over view</p>
+          <button class="btn">Move Commission</button>
+        </div>
+        <div class="box">
+          <div class="box1">
+            <div>
+              <i class="fal fa-wallet icons"></i>
+            </div>
+            <div>
+              <p>Wallet Balance</p>
+              <h3>N 2000.00</h3>
+            </div>
+          </div>
+          <div class="box1">
+            <div>
+              <i class="fas fa-cube icons"></i>
+              <!-- <i class="fal fa-wallet icons"></i> -->
+            </div>
+            <div>
+              <p>Commission Balance</p>
+              <h3>N 50.00</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="boximg">
+        <img src="images/graph.svg" alt="Graphics">
+      </div>
+      <diV class="box11">
+        <div class="box12">
+          <div>
+            <i class="far fa-home-lg-alt icons"></i>
+          </div>
+          <div>
+            <p>Transactions</p>
+            <h3>2</h3>
+          </div>
+        </div>
+        <div class="box13">
+          <div>
+            <i class="far fa-home-lg-alt icons"></i>
+          </div>
+          <div>
+            <p>Groups</p>
+            <h3>5</h3>
+          </div>
+        </div>
+        <div class="box14">
+          <div>
+            <i class="far fa-home-lg-alt icons"></i>
+          </div>
+          <div>
+            <p>Contacts</p>
+            <h3>10</h3>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div> */}
+        {/* <!--end second slide of the page--> */}
+      </div>
+    </>
+  );
+}
+
+export default Dashbord
