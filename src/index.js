41dd3ba65027b81d1208ee20b5a707b74dc514f3
morayodev <70142247+morayodev@users.noreply.github.com>
2021-02-19 15:00:51 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,34 +3,13 @@ import ReactDOM from 'react-dom';
 import './assets/css/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore,applyMiddleware,compose,combineReducers}  from "redux"
-// import auth from "./store/auth"
-import thunk from "redux-thunk";
+import {BrowserRouter} from 'react-router-dom';
 
-
-
-
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducer = combineReducers({
-  // burgerBuilder: reducer,
-  // actions: orderReducer,
-  // authReducer: auth,
-});
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
